ac934d59fbbb8257aa796eb85c20ee1b71b4dcd5
Meawckich <demidofficialzedmain@gmail.com>
2022-12-13 01:58:58 +0300

    publish commit

--- Scripts/codewarsApi.js
+++ Scripts/codewarsApi.js
@@ -1,9 +1,16 @@
 
 const getStats = async () => {
+    var name;
+    var katas;
+    var response = await fetch("https://www.codewars.com/api/v1/users/meawckich")
+    if(response.ok)
+    {
+        var json = await response.json();
+        document.getElementById('codewars').innerHTML = json.ranks.overall.name;
+        document.getElementById('katas').innerHTML = json.codeChallenges.totalCompleted;
+    }
+
     try{
-        fetch("https://www.codewars.com/api/v1/users/meawckich")
-        .then(response => response.json())
-        .then(result => document.getElementById('codewars').innerHTML = result.ranks.overall.name);
         fetch("https://api.github.com/users/meawckich")
         .then(response => response.json())
         .then(result => document.getElementById('github').innerHTML = result.public_repos == undefined? 0 : result.public_repos);
@@ -11,5 +18,4 @@ const getStats = async () => {
     catch (e) {
         throw new Error(e);
     }
-
 }
